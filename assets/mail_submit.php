411fb24d38c55ca52c819d75f801b030204d3281
QuinnLeavitt <qwe42@live.missouristate.edu>
2022-10-15 18:09:50 -0500

    Initial commit

---
+++ assets/mail_submit.php
@@ -0,0 +1,21 @@
+
+<?php 
+echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+if(isset($_POST['submit'])){
+    $to = "quinnleavitt@gamil.com"; 
+    $from = $_POST['email']; 
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    header('location:index.html');
+    }
+?>
+
+
+
+
